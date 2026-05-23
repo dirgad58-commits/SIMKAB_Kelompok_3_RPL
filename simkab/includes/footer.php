2c42b748cefb0dbb0100b0cac09126d1cb717c09
dirgad58-commits <dirgad58@gmail.com>
2026-05-23 13:20:35 +0800

    Add files via upload

---
+++ simkab/includes/footer.php
@@ -0,0 +1,10 @@
+    <!-- ==========================================================================
+         APPLICATION SCRIPTS
+         ========================================================================== -->
+    <!-- load Chart.js Configurations -->
+    <script src="js/chart-configs.js"></script>
+    
+    <!-- load Business Logic & DOM Events -->
+    <script src="js/app.js?v=2"></script>
+</body>
+</html>
